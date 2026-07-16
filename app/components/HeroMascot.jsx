93112d15531030d65d4ff1cda62fdb9838a84d88
termoseguro <termoseguro2021@gmail.com>
2026-07-15 23:34:22 -0300

    Troca animacao para MP4 com fundo rosa perfeito

--- app/components/HeroMascot.jsx
+++ app/components/HeroMascot.jsx
@@ -1,10 +1,12 @@
 export default function HeroMascot() {
   return (
     <div className="hero-mascot">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src="/hero-video.gif"
-        alt="Hero animation"
+      <video
+        src="/hero-video2.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
         style={{ width: "100%", height: "100%", objectFit: "contain", borderRadius: "18px" }}
       />
     </div>
